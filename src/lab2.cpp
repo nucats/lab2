9d72cb3e19bdc0a51f7017c3eb00731bda7e36f3
nucats <nishabhuva2018@u.northwestern.edu>
2016-01-13 14:49:35 -0600

    Initial Lab Session

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -1,24 +1,52 @@
-#include "eecs230.h"
 
-//
-// Lab 2 programs
-//
+#include <iostream>
 
-int illegalStatements() {
-    /* Start here! */
-    //int double = 0;
-    //double char = 2.5;
-    //char mychar = 'd'; mychar += 12;
-    //unsigned int a = -1;
-    //int pi = 3.14;
-    //short num = 1e9;
-    //string mystring = 5;
-    return 0;
+using namespace std;
+
+void odd_or_even (int num)
+{
+    if (num%2 == 0) //even
+        cout << "The value " << num << " is an even number" << endl;
+    else //odd
+        cout << "The value " << num << " is an odd number" << endl;
+}
+
+void spelled_out_numbers (char num1)
+{
+    if (num1 == zero)
+        cout << "0";
+    else if (num1 == one)
+            cout << "1";
+    else if (num1 == two)
+        cout << "2";
+    else if (num1 == three)
+        cout << "3";
+    else if (num1 == four)
+        cout << "4";
+    else
+        cout << "Not a number I know";
 }
 
 int main() {
-    illegalStatements();
-    cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
-    cout << "Then work on the exercises and code them up." << endl;
+ //   odd_or_even(7);
+    int age;
+    cout << "Enter the age of the recipient:" << endl;
+    cin >> age;
+
+
+    if (age<=0)
+    cout << "Error: You're kidding!";
+    else if (age>=110)
+    cout << "Error: You're kidding!";
+        else if (age<12)
+        cout << "Next year you will be " << age + 1 << endl;
+        else if (age==17)
+        cout << "Next year you will be able to vote." << endl;
+        else if (age>70)
+        cout << "I hope you are enjoying retirement." << endl;
+    else
+    cout << "I hear you just had a birthday and you are " << age << " years old." << endl;
+
 }
+
 
